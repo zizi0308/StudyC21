fc509f72051f49d988cddbe078bae55eb88286a3
JO HUI JI <whgmlwl222@naver.com>
2021-05-05 15:16:39 +0900

    20210505 코드수정 및 주석추가

--- Chap10/pointerExam.c
+++ Chap10/pointerExam.c
@@ -9,46 +9,46 @@ void print_ary(int* pa, int size);
 int main(void)
 {
 	int ary[10];
-	int size = sizeof(ary) / sizeof(ary[0]);		// 10개
-	input_ary(ary, size);
-	swap_ary(ary, size);
-	print_ary(ary, size);
-	printf("\n");
-	return 0;
+	int size = sizeof(ary) / sizeof(ary[0]);		// 10개, 배열요소의 개수를 계산하기 위해 씀
+	input_ary(ary, size);							// 값을 입력받기 위한 함수
+	swap_ary(ary, size);							// 값을 역순으로 바꾸기 위한 함수
+	print_ary(ary, size);							// 역순으로 바꾼 값을 출력하기 위한 함수
+	printf("\n");									// 개행
+	return 0;										// 메인 함수를 종료
 }
 
 void input_ary(int* pa, int size)
 {
-	int i;
+	int i;											// 반복문을 제어하기위한 변수
 
-	printf("%d개의 정수값 입력 : ", size);
+	printf("%d개의 정수값 입력 : ", size);			// 정수값을 입력받기 위해 넣음 size만큼 입력해야 함
 
-	for ( i = 0; i < size; i++)
+	for ( i = 0; i < size; i++)						// ary[0] 부터 ary[size-1]까지의 배열을 출력 
 	{
-		scanf("%d", &pa[i]);  // 정수를 받아야 하니까 scanf
+		scanf("%d", &pa[i]);						// 입력할 배열요소의 주소 전달(pa + i)도 가능
 	}
 }
 
 void swap_ary(int* pa, int size)
 {
-	int temp;
-	int i;
-	int j;
+	int temp;								// 값을 저장할 임시저장소
+	int i;									// 반복문을 제어하기 위한 변수
+	int j;									// 입력한 숫자배열을 역순대로 출력하기 위한 제어변수
 
-	for (i = 0; i < size / 2; i++)
-	{
-		j = size - i - 1;
-		temp = pa[i];
-		pa[i] = pa[j];
-		pa[j] = temp;
+	for (i = 0; i < size / 2; i++)			// 반만 바꾸면 역순으로 바뀜 전부 바꾸면 다시 원상복구되니까 /2를 함
+	{										// 숫자와 숫자가 서로 대칭인지를 봐야 하므로
+		j = size - i - 1;					// 배열의 끝으로 가기위해 
+		temp = pa[i];						// 임시저장변수에 pa[i]를 저장
+		pa[i] = pa[j];						// 배열요소의 교환, 첫번째 배열요소와 마지막 배열요소의 교환부터 순차적으로 이루어짐
+		pa[j] = temp;						// 교환 된 배열들을 다시 임시 저장소에 저장
 	}
 }
 void print_ary(int* pa, int size)
 {
 	int i;
 	
-	for ( i = 0; i < size; i++)
+	for ( i = 0; i < size; i++)				// pa[0]부터 pa[i-1]까지의 배열요소 돌면서
 	{
-		printf("%d ", pa[i]);
+		printf("%d ", pa[i]);				// 각 배열 요소들을 출력
 	}
 }
--- Chap14/회문.c
+++ Chap14/회문.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
-#include <string.h>
+#include <string.h>		// 문자열 관련 함수(strlen)를 사용하기위한 헤더파일 포함 
 
 int main(void)
 {
-	char str[10];
-	int i;
-	int str_len;
+	char str[10];		// char형 요소 10개의 배열선언
+	int i;				// 반복횟수를 세기 위한 제어 변수
+	int str_len;		// 문자열 길이를 표현하기 위한 변수
 	
+	printf("문자열을 입력하세요 : ");	// 처음 입력을 하기위한 안내
+	scanf("%s", str);					// 문자열로 입력받기 위해서 %s사용, 배열명은 주소이름이므로 주소연산자 필요없음
 	
-	printf("문자열을 입력하세요 : ");
-	scanf("%s", str);
-	str_len = strlen(str);
-	for ( i = 0; i < str_len / 2; i++)
+	str_len = strlen(str);				// 문자열의 길이를 표현하는 방법				
+	// 이게 위쪽에 있으면 입력받은 문자열이 아닌 char str[10]의 길이를 표현하므로 여기 있어야 함
+	
+	for ( i = 0; i < str_len / 2; i++)	// 문자열과 문자열이 대칭으로 바뀌기 때문에 절반만 루프를 돌림
 	{
-		if (str[i] != str[str_len - i - 1])
+		if (str[i] != str[str_len - i - 1])	// 처음과 끝의 문자열이 같지 않다면		
+		// str[str_len - i - 1] 인 이유 >> 전체(str_len)의 반(str_len / 2)을 쪼개서 배열 -1만큼 나타낸다(문자열의 마지막값은 null이므로)
 		{
-			//printf("%c%c", str[0], str[str_len - i -1]);
+			//printf("%c %c", str[0], str[str_len - i -1]); // 첫문자가 잘 들어갔는지 확인하기 위해 씀
 			printf("%s(은)는 회문이 아닙니다.", str);
-			break;
+			break; // for문이므로 반드시 break있어야 함 없을 시 회문아닙니다 반복적으로 뜸
 		}
 		printf("회문입니다.");
 	}
