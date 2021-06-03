962cbb306010f10d88300457c2c0d7a0ed855b85
JO HUI JI <whgmlwl222@naver.com>
2021-06-03 17:51:50 +0900

    20210603 C 과제 커밋

--- TestApp/FitnessManageProgram.c
+++ TestApp/FitnessManageProgram.c
@@ -1,5 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct {
 	int num;
@@ -14,35 +16,34 @@ int max_weight(Fitness** pary, int count);
 void print_info(Fitness** pary, int index);
 void free_ary(Fitness** pary, int count);
 
+
 int main()
 {
-	Fitness member[100];
-	int i;
+	static int count;	// 회원수
+	count = 0;			// 정적멤버변수 초기화
+	int i;				// 배열번호
 	i = 1;
-	int temp;
-	while (member[i].num > 0)
+	int temp = 0;
+	while (ary[i]->num > 0)
 	{
-		printf("회원번호, 이름, 체중을 입력하세요: ");
-		scanf("%d", temp);
+		printf("회원번호를 입력하세요: ");
+		scanf("%d", &temp);
 		if (temp < 0) {
 			break;
 		}
 		else {
-			member[i].num = temp;
-			scanf("%s %.1f", member[i].name, &member[i].weight);
+			ary[i]->num = temp;
+			printf("이름, 체중을 입력하세요: ");
+			scanf("%s %.1f", ary[i]->name, &ary[i]->weight);
 		}
-		
 	}
-
+	total_number(count);
+	average_weight(ary, count);
 	return 0;
 }
 void total_number(int count)
 {
-	int i;
-	for ( i = 1; i <= count; i++)
-	{
-		printf("전체회원 등록수는 %d명 입니다.", i);
-	}
+	printf("전체회원 등록수는 %d명 입니다.", count);
 }
 
 double average_weight(Fitness** pary, int count)
@@ -50,20 +51,29 @@ double average_weight(Fitness** pary, int count)
 	int i;
 	int sum;
 	sum = 0;
-	for ( i = 0; i <= count; i++)
-	{
-		sum += member[i].;
+	for (i = 0; i <= count; i++) {
+		sum += pary[i]->weight;
 	}
+	return sum / i;
 }
 
 int max_weight(Fitness** pary, int count)
 {
+	int max;
+	int i;
 
+	max = pary[0];
+	for (i = 0; i < count; i++)
+	{
+		if (pary[i] > max) max = pary[i];
+	}
+
+	return max;
 }
 
 void print_info(Fitness** pary, int index)
 {
-
+	printf("회원번호 : %d, 회원이름 : %s, 몸무게 : %.1lf", pary[index]->num, pary[index]->name, pary[index]->weight);
 }
 
 void free_ary(Fitness** pary, int count)
