e8df7d3b07ecb3db922854a2f9e103019c31a740
JO HUI JI <whgmlwl222@naver.com>
2021-06-06 19:10:38 +0900

    20210606 삽입정렬 문제풀이 커밋

--- Chapter7/dictionary.c
+++ Chapter7/dictionary.c
@@ -1,11 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
 
-
+void insertion_sort(char*);										// 포인터로 입력값을 받는 삽입정렬 함수생성
 int main()
 {
-	int input_ary[10];
-	int lower_ary[10];
-	int upper_ary[10];
+	int i = 0;													// for문을 위한 변수생성
+	char lower_ary[10] = {0};									// 소문자 저장배열 생성 및 초기화
+	char upper_ary[10] = {0};									// 대문자 저장배열 생성 및 초기화
+	char input_ary[10] = {0};									// 값을 입력받기 위한 배열생성 및 초기화
+	
+		printf("알파벳을 입력하세요 : ");
+		scanf("%s", input_ary);
+	
+	for ( i = 0; i < 10; i++)									// 배열의 9번째 방까지 i를 더해서 결과값 생성
+	{
+		if ((input_ary[i]) >= 'A' && (input_ary[i] <= 'Z'))		// input_ary가 대문자가 있으면
+		{
+			upper_ary[i] = input_ary[i];						// 그 값을 upper_ary에 저장함
+			//printf("대문자 %c", upper_ary[i]);				// 결과값 확인테스트
+		}
+		
+		if ((input_ary[i]) >= 'a' && (input_ary[i] <= 'z'))		// input_ary에 소문자가 있으면
+		{
+			lower_ary[i] = input_ary[i];						// 그 값을 lower_ary에 저장함
+			//printf("소문자 %c", lower_ary[i]);				// 결과값 확인테스트
+		}
+	}
+	
+	printf("<대문자 사전순 정렬>\n");
+	insertion_sort(upper_ary);
+	printf("\n");
+	printf("<소문자 사전순 정렬>\n");
+	insertion_sort(lower_ary);
+	printf("\n");
+	printf("<전체 사전순 정렬>\n");
+	insertion_sort(input_ary);
 
-	return 0;
+	return 0;													// 메인함수 종료
+}
+void insertion_sort(char *pinput_ary)
+{
+	int i;														// 비교하는 인덱스를 지정하기 위한 i
+	int j;														// 바로 앞 인덱스뿐만 아니라 앞의 전체 인덱스들을 비교하기위한 j
+	int temp = 0;												// swap을 위한 임시저장소
+	for ( i = 1; i < 10; i++)									// 두번째 값부터 비교가 시작되므로 인덱스가 1부터 시작해야 함, 전체 배열개수만큼 실행
+	{
+		for (j = i; j > 0; j--) {								// j가 왼쪽으로 이동해야 하므로(앞의 인덱스들을 비교) 점점 감소됨
+			if (pinput_ary[j] < pinput_ary[j - 1]) {			// j번 배열의 알파벳보다 j-1번 배열의 알파벳이 더 크다면
+				temp = pinput_ary[j];							// j를 임시저장한다(정렬해야 할 대상을 임시저장함)
+				pinput_ary[j] = pinput_ary[j - 1];				// j의 자리에 다시 j-1을 넣어주고
+				pinput_ary[j - 1]  = temp;						// j-1의 자리에 temp의 값(j)을 넣어주면서 서로 자리가 바뀌고 정렬이 된다.
+			}
+		}
+	}
+	for (i = 0; i < 10; i++) {									// 출력
+		printf("%c", pinput_ary[i]);
+	}
 }
--- TestApp/FitnessManageProgram.c
+++ TestApp/FitnessManageProgram.c
@@ -3,10 +3,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct {
-	int num;
-	char name[20];
-	double weight;
+typedef struct {									// 구조체 선언
+	int num;										// int형 멤버변수
+	char name[20];									// 문자열 멤버변수
+	double weight;									// double형 멤버변수
 } Fitness;
 
 Fitness* ary[100];									// 포인터 배열선언
@@ -22,80 +22,83 @@ int main()
 	static int count;	// 회원수
 	count = 0;			// 정적멤버변수 초기화
 	int i;				// 배열번호
-	i = 0;
-	int temp = 0;
+	i = 0;				// 배열번호 초기화
+	int temp = 0;		// 회원번호 임시저장을 위한 변수생성
 
 	while (1)
 	{
 		printf("회원번호를 입력하세요: ");						// 입력 메세지
 		scanf("%d", &temp);										// 회원번호 입력
-		if (temp < 0) break;									// temp가 0미만이면 break
+		if (temp < 0) break;									// temp가 0미만이면(회원번호 음수입력이면) break
 		else {
-			ary[i] = malloc(sizeof(Fitness));			// 회원번호 동적할당
-			if (ary[i] != NULL) {							// 회원번호가 존재하면
-				ary[i]->num = temp;						// ary[i]->num에 temp를 복사해준다
-				printf("이름을 입력하세요: ");				// 입력 메세지
-				scanf("%s", ary[i]->name);	// 이름, 몸무게 입력
-				printf("체중을 입력하세요: ");
-				scanf("%lf", &ary[i]->weight);	// 이름, 몸무게 입력
-				getchar();
-				i++;
-				count++;
+			ary[i] = malloc(sizeof(Fitness));					// 배열 동적할당
+			if (ary[i] != NULL) {								// 배열이 존재하면
+				ary[i]->num = temp;								// temp에 ary[i]->num를 복사해준다
+				printf("이름을 입력하세요: ");					// 입력 메세지(이름)
+				scanf("%s", ary[i]->name);						// 이름 입력
+				printf("체중을 입력하세요: ");					// 입력 메세지(체중)
+				scanf("%lf", &ary[i]->weight);					// 몸무게 입력
+				printf("\n");
+				getchar();										// 버퍼를 비우기 위한 getchar()
+				i++;											// 증감연산자 i(무한반복으로 break되기 전까지 순차적으로 배열을 증가시킴)
+				count++;										// 증감연산자 count(무한반복으로 break되기 전까지 순차적으로 회원수를 증가시킴)
 			}
 		}
 	}
-	total_number(count);
-	printf("평균몸무게 : %lf\n", average_weight(ary, count));
-	printf("최고몸무게 인덱스 : %d\n", max_weight(ary, count));
-	//average_weight(ary, count);
-
-	print_info(ary, count - 1);
-	free_ary(ary, count);
-	return 0;
+	printf("\n");
+	total_number(count);											// 회원수를 반환하기위해 입력값에 count를 넣어줌
+	printf("평균몸무게 : %lf kg\t", average_weight(ary, count));	// 평균몸무게 출력(average_weight의 출력형이 double이므로 %lf)
+	printf("최고몸무게 회원번호 : %d\n", max_weight(ary, count));	// 최고 몸무게 회원번호 출력
+	print_info(ary, count - 1);										// count는 0부터 시작, index는 1부터 시작(Line 87)하므로 count - 1을 해줌
+	free_ary(ary, count);											// 배열의 동적할당 해제
+	return 0;														// 메인함수 종료
 }
+
 void total_number(int count) {
-	printf("전체회원 등록수는 %d명 입니다.\n", count);
+	printf("전체회원 등록수: %d명\n", count);
 }
 
 double average_weight(Fitness** pary, int count)
 {
-	int i;														// for문 실행을 위한 변수
-	double sum;													// 임시 합계변수
-	sum = 0;													// 초기화
-	for (i = 0; i < count; i++) {								// i가 count와 같을 때까지 계속 반복
-		sum += pary[i]->weight;									// sum에 pary[i]->weight의 값들을 계속 더해줌 
+	int i;														// for문 실행을 위한 변수 i
+	double sum;													// 합계변수 sum
+	sum = 0;													// sum 초기화
+	for (i = 0; i < count; i++) {								// i가 count-1과 같을 때까지 계속 반복
+		sum += pary[i]->weight;									// pary[i]->weight의 값들을 계속 더해 sum에 저장함 
 	}
-	return sum / i;											// 총합 / 개수 + 1(i는 0부터 시작)를 return해줌
+	return sum / i;												// 총합 / 개수를 return해줌
 }
 
 int max_weight(Fitness** pary, int count)
 {
-	double max_weight;
-	int i;
-	int max_index;
+	double max_weight;											// 최고 몸무게를 저장하기 위한 변수
+	int i;														// for문 실행을 위한 변수
+	int max_index;												// 최고 몸무게의 인덱스 값을 저장하기 위한 변수
 
-	max_index = 0;
-	max_weight = pary[max_index]->weight;										// 첫번째 값을 최대몸무게로 설정
+	max_index = 0;												// max_index 초기화
+	max_weight = pary[max_index]->weight;						// 첫번째 값을 최대몸무게로 설정
 	
-	for (i = 0; i < count; i++) {								// count까지 for문 실행
-		if (pary[i]->weight > max_weight) {
-			max_weight = pary[i]->weight;		// 해당 배열의 몸무게 데이터가 max보다 크면 해당배열을 max로 재설정
-			max_index = i;
+	for (i = 0; i < count; i++) {								// count - 1까지 for문 실행
+		if (pary[i]->weight > max_weight) {						// 해당 배열의 몸무게 데이터가 max_weight보다 크면
+			max_weight = pary[i]->weight;						// 해당 배열을 최대몸무게로 재설정하고
+			max_index = i;										// 그 최대 몸무게의 인덱스 값을 max_index에 저장
 		}
 	}
-	return max_index;													// 최대 몸무게의 인덱스 값을 반환하는 함수이므로 i값을 반환
+	return max_index;											// for문 실행이후 저장된 최대몸무게의 인덱스 반환
 }
 
 void print_info(Fitness** pary, int index)
 {
-	printf("회원번호 : %d , 회원이름 : %s , 몸무게 : %f\n", pary[index]->num, pary[index]->name, pary[index]->weight);
+	printf("\n");
+	printf("<회원정보>\n");
+	printf("회원번호 : %d\t  회원이름 : %s\t  몸무게 : %f kg\n", pary[index]->num, pary[index]->name, pary[index]->weight);
 }
 
 void free_ary(Fitness** pary, int count)
 {
-	int i;
-	for (i = 0; i < count; i++) {	// 동적할당을 했던 pary[i]->num에 대해 동적할당 해제를 시킴
-		free(pary[i]);
-		printf("%d번째완료", i);
+	int i;														// for문 실행을 위한 변수 i
+	for (i = 0; i < count; i++) {								// 동적할당을 했던 pary[i]에 대해 for문을 돌며 순차적으로 
+		free(pary[i]);											// 동적할당을 해제를 시킴
+		//printf("%d번째완료", i);								// 동적할당 해제 확인
 	}
 }
